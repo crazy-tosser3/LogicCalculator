7358e23b0b7d3a6cd8bdfcbda104adc62c583a5d
crazy-tosser3 <crazytosser3@gmail.com>
2025-09-15 21:33:09 +0300

    New Func

--- js/button.js
+++ js/button.js
@@ -2,3 +2,16 @@ function addText(button) {
     var inputfield = document.getElementById("expression")
     inputfield.value += button.innerText;
 }
+
+function showMessage() {
+    const shadow = document.querySelector('#shadow');
+    const message = document.querySelector('#message');
+
+    shadow.style.display = 'block';
+    message.style.display = 'block';
+
+    shadow.addEventListener('click', () => {
+        shadow.style.display = 'none';
+        message.style.display = 'none';
+    }, { once: true });
+}
